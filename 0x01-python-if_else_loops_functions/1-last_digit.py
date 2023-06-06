720c6a1611b4648c395fbe1c2cced94637d40a42
Tinodana <abinarac@gmail>
2023-06-06 13:40:36 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
+#import random
+#number = random.randint(-10000, 10000)
+#if int(str(number)[-1]) > 5:
+#    print(f'Last digit of {number} is ' + f'{number}'[-1] + ' and is greater than 5')
+#elif int(str(number)[-1]) > 5 and number < 0:
+#    print(f'Last digit of {number} is - ' + f'-{number}'[-1] + ' and is greater than 5')
+#elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
+#    print(f'Last digit of {number} is '+f'{number}'[-1] +' and is less than 6 and not 0')
+#elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0 and number < 0:
+#    print(f'Last digit of {number} is - '+f'{number}'[-1] +' and is less than 6 and not 0')
+#elif int(str(number)[-1]) == 0:
+#    print(f'Last digit of {number} is ' + f'{number}'[-1] + ' and is 0')'
+
 import random
 number = random.randint(-10000, 10000)
-if int(str(number)[-1]) > 5:
-    print(f'Last digit of {number} is ' + f'{number}'[-1] + ' and is greater than 5 and not 0')
-elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
-    print(f'Last digit of {number} is ' + f'{number}'[-1] + ' and is less than 6 and not 0')
-elif int(str(number)[-1]) == 0:
+
+if int(str(number)[-1]) == 0:
     print(f'Last digit of {number} is ' + f'{number}'[-1] + ' and is 0')
+elif number < 0:
+    print(f'Last digit of {number} is -' + f'{number}'[-1] + ' and is less than 6 and not 0')
+elif int(str(number)[-1]) > 5:
+    print(f'Last digit of {number} is ' + f'{number}'[-1] + ' and is greater than 5')
+elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
+    print(f'Last digit of {number} is '+f'{number}'[-1] +' and is less than 6 and not 0')
+#elif number < 0 and int(str(number)[-1]) == 0:
+#    print(f'Last digit of {number} is ' + f'{number}'[-1] + ' and is 0')
